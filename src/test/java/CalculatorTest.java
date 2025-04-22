446762bd2d5ee2acac003cf762fe720ab696c8cf
sergey <wendingnn@gmail.com>
2025-04-22 11:44:27 +0300

    realese calculator

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,31 +1,43 @@
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 
+    private final Calculator calculator = new Calculator();
+
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        assertEquals(5, calculator.add(2, 3));
+        assertEquals(-1, calculator.add(-2, 1));
+        assertEquals(0, calculator.add(0, 0));
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        assertEquals(-1, calculator.dif(2, 3));
+        assertEquals(-3, calculator.dif(-2, 1));
+        assertEquals(0, calculator.dif(0, 0));
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        assertEquals(2, calculator.div(6, 3));
+        assertEquals(-2, calculator.div(-4, 2));
+
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        assertEquals(6, calculator.times(2, 3));
+        assertEquals(-6, calculator.times(-2, 3));
+        assertEquals(0, calculator.times(0, 5));
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        assertEquals(5, calculator.solver(2, 3, "add"));
+        assertEquals(-1, calculator.solver(2, 3, "dif"));
+        assertEquals(2, calculator.solver(6, 3, "div"));
+        assertEquals(6, calculator.solver(2, 3, "times"));
     }
 }
